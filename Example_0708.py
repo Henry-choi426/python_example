53d823e031bceaff3d7c4454da36d9fb0d536cde
최한승 <84162474+Henry-choi426@users.noreply.github.com>
2021-07-28 01:08:23 +0900

    Update Example_0708.py
    수정 완료

--- Example_0708.py
+++ Example_0708.py
@@ -9,13 +9,19 @@
 """
 def solution(floor):
 
-    answer = [0] # 시작점 점수
+    answer = [] # 시작점 점수
     floor.reverse() # 마지막 도착 계단을 무조건 밟아야 하므로 뒤에서부터 내려오기
-    answer.append(floor[0]) # 1층 sum의 최댓값 미리 넣어주기 
-
-    for i in range(1,len(floor)-1):
-        answer.append(max(answer[i],answer[i-1]+floor[i]))
-
+    answer.append(floor[0]) # 도착 지점의 계단값으로 sum의 최댓값 미리 넣어주기 
+    
+    answer.append(floor[0]+floor[1])
+    answer.append(max(floor[0]+floor[2],floor[1]+floor[2]))
+    
+    print(answer)
+    
+    for i in range(3,len(floor)):
+        answer.append(max(answer[i-2]+floor[i],answer[i-3]+floor[i-1]+floor[i]))
+    print(answer)
+    print(floor)
     return answer[-1]
     
-solution([6,10,20,15,25,10,20]) # 65
+print(solution([6,10,20,15,25,10,20])) # 76
